a6afcf8541f533a51c1a2c36bd8f4ac67a8e13be
thinnm00 <thinnguyen332000@gmail.com>
2025-04-28 15:15:42 +0700

    split url source list to a new file

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import pandas as pd
 import openpyxl
 from openpyxl.styles import Font, PatternFill, Alignment
 from datetime import datetime
+from source_url import urls
 
 from crawl4ai import AsyncWebCrawler
 from crawl4ai.async_configs import BrowserConfig, CrawlerRunConfig, CacheMode
@@ -45,17 +46,7 @@ async def process_gemini_response(response_text):
 async def get_job_info(data, url):
     genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
     model = genai.GenerativeModel("gemini-2.0-flash")
-    
-    # prompt = f"""{url}\n{data}\nHãy phân tích và trích xuất thông tin Việc Làm được giới thiệu ở trang web này, chuyển về dạng JSON với cấu trúc:\n
-    # {{
-    #     "jobname": "$Tên Việc Làm đang được giới thiệu ở trang web này",
-    #     "companyname": "$Tên Công ty đang được giới thiệu ở trang web này",
-    #     "url": "$URL href (của công việc đó)",
-    #     "expired": "$Thời gian hiệu lực của công việc đang tuyển dụng đó"
-    #     "contact": "$Thông tin liên hệ của công ty tuyển dụng là số điện thoại hoặc email nếu có"
-    # }}
-    # """
-    
+        
     prompt = f"""{url}\n{data}\nHãy phân tích và trích xuất thông tin Việc Làm được giới thiệu ở trang web này, chuyển về dạng JSON với cấu trúc:
 
     {{
@@ -88,15 +79,8 @@ async def main():
     all_jobs = []  # Tạo list để lưu tất cả job_info
 
     async with AsyncWebCrawler(config=browser_config) as crawler:
-        urls = [
-            "https://www.topcv.vn/tim-viec-lam-it-support-tai-binh-duong-kl3?sba=1&locations=l3",
-            "https://careerviet.vn/viec-lam/it-tai-binh-duong-kl650-vi.html",
-            "https://www.vietnamworks.com/viec-lam?q=it&l=11",
-            #"https://vn.indeed.com/jobs?q=it&l=B%C3%ACnh+D%C6%B0%C6%A1ng&from=searchOnHP%2Cwhereautocomplete&vjk=d110fd014c9f4c77",
-            "https://jobsgo.vn/viec-lam-cong-nghe-thong-tin-tai-binh-duong.html",
-            "https://www.jobstreet.vn/j?sp=homepage&trigger_source=homepage&q=it&l=B%C3%ACnh+D%C6%B0%C6%A1ng",
-        ]
-
+        
+        # duyệt từng url trong danh sách
         for url in urls:
             try:    
                 result = await crawler.arun(url=url, config=run_config)
---
+++ source_url.py
@@ -0,0 +1,9 @@
+# python file store url list for easy adding new urls or maintenance
+
+urls = [
+    "https://www.topcv.vn/tim-viec-lam-it-support-tai-binh-duong-kl3?sba=1&locations=l3",
+    "https://careerviet.vn/viec-lam/it-tai-binh-duong-kl650-vi.html",
+    "https://www.vietnamworks.com/viec-lam?q=it&l=11",
+    "https://jobsgo.vn/viec-lam-cong-nghe-thong-tin-tai-binh-duong.html",
+    "https://www.jobstreet.vn/j?sp=homepage&trigger_source=homepage&q=it&l=B%C3%ACnh+D%C6%B0%C6%A1ng",
+]
