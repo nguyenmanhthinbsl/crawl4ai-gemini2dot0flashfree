66b25016d7597d673d7215ddbc3d17a3c6e0371a
thinnm00 <thinnguyen332000@gmail.com>
2025-05-21 15:17:23 +0700

    add more url target

--- source_url.py
+++ source_url.py
@@ -6,4 +6,5 @@ urls = [
     "https://www.vietnamworks.com/viec-lam?q=it&l=11",
     "https://jobsgo.vn/viec-lam-cong-nghe-thong-tin-tai-binh-duong.html",
     "https://www.jobstreet.vn/j?sp=homepage&trigger_source=homepage&q=it&l=B%C3%ACnh+D%C6%B0%C6%A1ng",
+    "https://www.google.com/search?q=vi%E1%BB%87c%20l%C3%A0m%20it%20b%C3%ACnh%20d%C6%B0%C6%A1ng&rlz=1C1CHZN_viVN1150VN1150&oq=vi%E1%BB%87c%20l%C3%A0m%20it&gs_lcrp=EgZjaHJvbWUqBwgAEAAYgAQyBwgAEAAYgAQyBggBEEUYOTIHCAIQABiABDIHCAMQABiABDIHCAQQABiABDIGCAUQRRg8MgYIBhBFGDwyBggHEEUYPNIBCDI3MDlqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8&jbr=sep:0&udm=8&ved=2ahUKEwiFnqGCrbONAxWFslYBHbrkHWkQ3L8LegQIKRAN"
 ]
